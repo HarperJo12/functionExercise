d4e17a82429bd21bef903e9e00830ee313e0d16f
HarperJo12 <68649970+HarperJo12@users.noreply.github.com>
2021-01-27 03:48:59 +0700

    Update exerciseFunctions.py

--- exerciseFunctions.py
+++ exerciseFunctions.py
@@ -2,7 +2,7 @@ def highest_even(li):
     max = 0
     for item in li:
         if item % 2 == 0 and item > max:
-            max += item
+            max = item
     return max
 
 print(highest_even([10,2,3,4,8,11]))
